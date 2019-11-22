b9c39fc2efb18efe162c83e03f3f3649f7e586f4
Dante Razo <dante@razonet.org>
2019-11-22 07:00:49 -0500

    Change test split (Founta)

--- A7 - Paper SVMs/Founta/founta_svm.py
+++ A7 - Paper SVMs/Founta/founta_svm.py
@@ -30,7 +30,7 @@ def get_data():
     print(hate[1:5])  # debugging
     print(f"shape: {len(y)}")
 
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
     data = pd.read_csv(f"{data_dir}/hate.txt", sep='\t')
     train = data.loc[data['split'] == "train"]
