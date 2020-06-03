7798e6e8df1b84d25cfec69b1dd9068587840439
Dante Razo <dante@razonet.org>
2020-06-03 18:36:35 -0400

    Lexicon. Restructure lexicon folder + move manual_lexicon build functions to `build` + build new manual lexicon with ONLY abusive words

--- LING-X490-SU20/kaggle_build.py
+++ LING-X490-SU20/kaggle_build.py
@@ -1,12 +1,12 @@
 # LING-X 490
 # This file builds, imports, and exports data
 # Dante Razo, drazo
-from kaggle_preprocessing import read_data, boost_data, sample_data
+from kaggle_preprocessing import read_data, boost_data, sample_data, boost_multithreaded
 import pandas as pd
+import numpy as np
 import os
+import glob
 
-
-# data headers: [y, X]
 
 # only import once
 def get_train():
@@ -61,7 +61,7 @@ def export_df(data, sample="no_sample", i="", path="", prefix="", index=True):
 
 
 # builds one or both
-def build_main(choice, topic, repeats, sample_size, verbose):
+def build_train_samples(choice, topic, repeats, sample_size, verbose):
     """
     choice: choose which sample types to build. "random", "boosted", or "all"
     topic: topic for manual boosting
@@ -73,18 +73,101 @@ def build_main(choice, topic, repeats, sample_size, verbose):
     print(f"Datasets built.") if verbose else None
 
 
-# import Wiegand's lexicons, format them, and export them
-# in `kaggle_build.py` because it isn't dynamic, i.e. the output is the same after every run
-def build_wiegand_lexicons():
+def build_manual_lexicon():
     cwd = os.getcwd()
 
-    data_dir = "../repos/lexicon-of-abusive-words/lexicons"  # common directory for all repos. assumes local sys
+    os.chdir("lexicon_manual")
+    files = glob.glob('*.{}'.format("csv")) + glob.glob('*.{}'.format("tsv"))
+    dfs = []
 
-    # read lexicons
-    base = pd.read_csv(f"{data_dir}/baseLexicon.txt", sep='\t', header=None, names=["word", "class"])  # import base
-    exp = pd.read_csv(f"{data_dir}/expandedLexicon.txt", sep='\t', header=None, names=["word", "score"])  # expanded
+    # assumes they're all the same length (551, as was the provided lexicon)
+    for filename in files:
+        author = filename.split(".")[-2].strip()
 
-    # split word and part-of-speech
+        if author == "dante":
+            dfs.append(lexicon_dante(filename))
+        elif author == "dd":
+            dfs.append(lexicon_dd(filename))
+        elif author == "schaede":  # schaede
+            dfs.append(lexicon_schaede(filename))
+
+    df = pd.concat(dfs, axis=1)  # one big dataframe
+    df = df.loc[:, ~df.columns.duplicated()]  # remove duplicate "word" columns
+    df["avg"] = df[df.columns[1:]].mean(axis=1)  # average class columns
+    df["var"] = df[df.columns[1:]].var(axis=1)  # variance of class columns
+
+    df["class"] = False
+    df.loc[df.avg > 0.6, "class"] = True  # i.e. at least 2 people say its mildly
+    export_df(df, sample="all", prefix="lexicon.manual")  # export it too
+
+    os.chdir(cwd)  # return to previous cwd
+    return df  # just in case
+
+
+# manual lexicon helper f()
+def lexicon_dante(filename):
+    df = pd.read_csv(filename)[["word", "pass2"]]
+    df.columns = ["word", "dante"]
+    return df
+
+
+# manual lexicon helper f()
+def lexicon_dd(filename):
+    df = pd.read_csv(filename, sep='\t', header=0)[["word", "opinion"]]
+
+    class_vec = []
+    for x in df["opinion"]:
+        manual_class = str(x).lower()
+
+        if manual_class == "very abusive":
+            class_vec.append(2)
+        elif manual_class == "mildly abusive":
+            class_vec.append(1)
+        elif manual_class == "not abusive":
+            class_vec.append(0)
+        else:
+            class_vec.append(np.NaN)
+
+    df["dd"] = class_vec
+    df = df[["word", "dd"]]
+    return df
+
+
+# manual lexicon helper f()
+def lexicon_schaede(filename):
+    df = pd.read_csv(filename, header=0).iloc[:, 0:2]
+    df.columns = ["word", "opinion"]
+
+    class_vec = []
+    for x in df["opinion"]:
+        manual_class = str(x).lower()
+
+        if manual_class == "very abusive":
+            class_vec.append(2)
+        elif manual_class == "mildly abusive":
+            class_vec.append(1)
+        elif manual_class == "not abusive":
+            class_vec.append(0)
+        else:
+            class_vec.append(np.NaN)
+
+    df["schaede"] = class_vec
+    df = df[["word", "schaede"]]
+    return df
+
+
+# import lexicons, format them, and export them
+# in `kaggle_build.py` because it isn't dynamic, i.e. the output is the same after every run
+def export_lexicons():
+    cwd = os.getcwd()
+    os.chdir("../data/kaggle_data")
+
+    # read lexicons; Wiegand lexicons are provided and don't need to be built
+    base = pd.read_csv(f"lexicon_wiegand/baseLexicon.txt", sep='\t', header=None, names=["word", "class"])
+    exp = pd.read_csv(f"lexicon_wiegand/expandedLexicon.txt", sep='\t', header=None, names=["word", "score"])
+    rds = build_manual_lexicon()  # our manually-tagged lexicon; based off Wiegand's base lexicon
+
+    # split word and part-of-speech (Wiegand only)
     base_split = [w.split("_") for w in base["word"]]
     exp_split = [w.split("_") for w in exp["word"]]
 
@@ -94,27 +177,27 @@ def build_wiegand_lexicons():
     exp["part"] = [s[1] for s in exp_split]
     exp["word"] = [s[0] for s in exp_split]
 
-    # base-specific
+    # filter out non-abusive words
     base_abusive = base[base["class"]]["word"]
-
-    # expanded-specific. any word with a score >0 is considered abusive in their expanded lexicon
-    #   ref: Introducing a Lexicon of Abusive Words (Wiegand, Ruppenhofer, Schmidt, Greenberg)
-    exp_abusive = exp[exp["score"] > 0]["word"]
+    exp_abusive = exp[exp["score"] > 0]["word"]  # according to "Introducing a Lexicon of Abusive Words"
+    rds_abusive = rds[rds["class"]]["word"]
 
     # export
-    os.chdir("../data/kaggle_data/lexicon")
+    os.chdir("lexicon_wiegand")
     base.to_csv("lexicon.wiegand.base.csv", header=0, index=False)
     exp.to_csv("lexicon.wiegand.expanded.csv", header=0, index=False)
     base_abusive.to_csv("lexicon.wiegand.base.abusive.csv", header=0, index=False)
     exp_abusive.to_csv("lexicon.wiegand.expanded.abusive.csv", header=0, index=False)
+
+    os.chdir("../lexicon_manual")
+    rds_abusive.to_csv("lexicon.manual.all.abusive.csv", header=0, index=False)
+
     os.chdir(cwd)  # go back to previous cwd
 
 
-""" MAIN """
-# configuration
-topic = ["trump"]  # [str]
-to_build = "all"  # "all", "random", or "boosted"
-
-# manually run:
-# build_main(to_build, topic)
-# build_wiegand_lexicons()
+if __name__ == '__main__':
+    topic = ["trump"]  # [str]
+    to_build = "all"  # "all", "random", or "boosted"
+    # build_train_samples(to_build, topic)
+    export_lexicons()
+    pass
--- LING-X490-SU20/kaggle_postprocessing.py
+++ LING-X490-SU20/kaggle_postprocessing.py
@@ -1,41 +1,10 @@
 # LING-X 490
 # This standalone file takes built data and reformats / averages / analyzes it
 # Dante Razo, drazo
-from kaggle_build import export_df, get_train
 from kaggle_preprocessing import boost_multithreaded
 from multiprocessing import Process, Queue
 import pandas as pd
-import numpy as np
-import os
-import glob
 
-
-def compare_lexicons():
-    path = "../data/kaggle_data/lexicon_manual/"
-
-    os.chdir(path)
-    files = glob.glob('*.{}'.format("csv")) + glob.glob('*.{}'.format("tsv"))
-    dfs = []
-
-    # assumes they're all the same length (551, as was the provided lexicon)
-    for filename in files:
-        author = filename.split(".")[-2].strip()
-
-        if author == "dante":
-            dfs.append(lexicon_dante(filename))
-        elif author == "dd":
-            dfs.append(lexicon_dd(filename))
-        elif author == "schaede":  # schaede
-            dfs.append(lexicon_schaede(filename))
-
-    df = pd.concat(dfs, axis=1)  # one big dataframe
-    df = df.loc[:, ~df.columns.duplicated()]  # remove duplicate "word" columns
-    df["avg"] = df[df.columns[1:]].mean(axis=1)  # average class columns
-    df["var"] = df[df.columns[1:]].var(axis=1)  # variance of class columns
-
-    df["class"] = False
-    df.loc[df.avg > 0.6, "class"] = True  # i.e. at least 2 people say its mildly
-    export_df(df, sample="all", prefix="lexicon.manual")  # export it too
 
 
 # calculate % examples in given data that contains abusive words. returns list
@@ -45,8 +14,8 @@ def percent_abusive(data, lex):
     lex (str): lexicon to filter with. Either "we" (wiegand extended) or "rds" (our manually tagged dataset)
     """
     jobs = []
-    names = ["RDS (Manual), Wiegand E"]
-    to_return = []
+    lexicon_names = ["Manual", "Wiegand (Base)", "Wiegand (Expanded)"]
+    percentages = []
     q = Queue()
 
     filename = "../data/kaggle_data/lexicon_manual/lexicon.manual.all.csv"
@@ -57,79 +26,35 @@ def percent_abusive(data, lex):
     jobs.append(p1)
     p1.start()
 
-    filename = "../Data/kaggle_data/lexicon/lexicon.wiegand.base.abusive.csv"
-    lexicon_wiegand = pd.read_csv(filename, sep=",", header=0)
-    boost_list = list(lexicon_wiegand["word"])  # todo: actually use expanded lexion
+    filename = "../Data/kaggle_data/lexicon_wiegand/lexicon.wiegand.base.abusive.csv"
+    lexicon_wiegand_base = pd.read_csv(filename, sep=",", header=0)
+    boost_list = list(lexicon_wiegand_base["word"])
     p2 = Process(target=boost_multithreaded, args=(data, filename, boost_list, q,))
     jobs.append(p2)
     p2.start()
+
+    filename = "../Data/kaggle_data/lexicon_wiegand/lexicon.wiegand.expanded.abusive.csv"
+    lexicon_wiegand_exp = pd.read_csv(filename, sep=",", header=0)
+    boost_list = list(lexicon_wiegand_exp["word"])
+    p3 = Process(target=boost_multithreaded, args=(data, filename, boost_list, q,))
+    jobs.append(p3)
+    p3.start()
 
     # multithreaded boosting; waits for all jobs to finish
     for process in jobs:
         process.join()
 
     # get output
-    for j in jobs:
+    for x in jobs:
         boosted_df = q.get()
-        to_return.append(round(len(boosted_df) / len(data) * 100, 2))
+        percentages.append(round(len(boosted_df) / len(data) * 100, 2))
+
+    to_return = list(zip(lexicon_names, percentages))
+    print(to_return)  # DEBUG
 
     return to_return
 
 
-""" CLEAN IMPORTED DATA """
-
-
-# csv with extra columns
-def lexicon_dante(filename):
-    df = pd.read_csv(filename)[["word", "pass2"]]
-    df.columns = ["word", "dante"]
-    return df
-
-
-# ssv
-def lexicon_dd(filename):
-    df = pd.read_csv(filename, sep='\t', header=0)[["word", "opinion"]]
-
-    class_vec = []
-    for x in df["opinion"]:
-        manual_class = str(x).lower()
-
-        if manual_class == "very abusive":
-            class_vec.append(2)
-        elif manual_class == "mildly abusive":
-            class_vec.append(1)
-        elif manual_class == "not abusive":
-            class_vec.append(0)
-        else:
-            class_vec.append(np.NaN)
-
-    df["dd"] = class_vec
-    df = df[["word", "dd"]]
-    return df
-
-
-# csv
-def lexicon_schaede(filename):
-    df = pd.read_csv(filename, header=0).iloc[:, 0:2]
-    df.columns = ["word", "opinion"]
-
-    class_vec = []
-    for x in df["opinion"]:
-        manual_class = str(x).lower()
-
-        if manual_class == "very abusive":
-            class_vec.append(2)
-        elif manual_class == "mildly abusive":
-            class_vec.append(1)
-        elif manual_class == "not abusive":
-            class_vec.append(0)
-        else:
-            class_vec.append(np.NaN)
-
-    df["schaede"] = class_vec
-    df = df[["word", "schaede"]]
-    return df
-
-
-""" MAIN """
-# compare_lexicons()
+if __name__ == '__main__':
+    # build_manual_lexicon()
+    pass
