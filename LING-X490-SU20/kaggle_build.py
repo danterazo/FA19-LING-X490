e26d6ce3d42d2774c4b5fdbd33da8f76a7d6eff0
Dante Razo <dante@razonet.org>
2020-06-03 18:00:26 -0400

    Rebuild Wiegand lexicons

--- LING-X490-SU20/kaggle_build.py
+++ LING-X490-SU20/kaggle_build.py
@@ -73,36 +73,41 @@ def build_main(choice, topic, repeats, sample_size, verbose):
     print(f"Datasets built.") if verbose else None
 
 
-# import Wiegand's lexicon, format it, and export it
+# import Wiegand's lexicons, format them, and export them
 # in `kaggle_build.py` because it isn't dynamic, i.e. the output is the same after every run
-def build_lexicon():
-    """ GOAL: create three-dimensional data
-    1. Word
-    2. Part of speech
-    3. Class
+def build_wiegand_lexicons():
+    cwd = os.getcwd()
 
-    Then, manually remove non-abusive examples
-    """
     data_dir = "../repos/lexicon-of-abusive-words/lexicons"  # common directory for all repos. assumes local sys
-    dataset = "base"  # base | expanded
 
-    names = ["word", "class"]
-    data = pd.read_csv(f"{data_dir}/{dataset}Lexicon.txt", sep='\t', header=None, names=names)  # import Kaggle data
+    # read lexicons
+    base = pd.read_csv(f"{data_dir}/baseLexicon.txt", sep='\t', header=None, names=["word", "class"])  # import base
+    exp = pd.read_csv(f"{data_dir}/expandedLexicon.txt", sep='\t', header=None, names=["word", "score"])  # expanded
 
-    split = [w.split("_") for w in data["word"]]  # split word and PoS
+    # split word and part-of-speech
+    base_split = [w.split("_") for w in base["word"]]
+    exp_split = [w.split("_") for w in exp["word"]]
 
-    data["part"] = [s[1] for s in split]  # remove PoS from words
-    data["word"] = [s[0] for s in split]
+    # new dfs
+    base["part"] = [s[1] for s in base_split]
+    base["word"] = [s[0] for s in base_split]
+    exp["part"] = [s[1] for s in exp_split]
+    exp["word"] = [s[0] for s in exp_split]
 
-    data.to_csv("lexicon_wiegand.csv", index=False)  # save to `.csv`
+    # base-specific
+    base_abusive = base[base["class"]]["word"]
 
-    # also export a lexicon of ONLY abusive words
-    abusive = data[data["class"]]
-    abusive["manual"] = ""
-    abusive = abusive[["word", "class", "manual"]]
+    # expanded-specific. any word with a score >0 is considered abusive in their expanded lexicon
+    #   ref: Introducing a Lexicon of Abusive Words (Wiegand, Ruppenhofer, Schmidt, Greenberg)
+    exp_abusive = exp[exp["score"] > 0]["word"]
 
-    filepath = os.path.join("../data/kaggle_data/lexicon", "lexicon.wiegand.abusive.csv")
-    abusive.to_csv(filepath, index=False)  # save to `.csv`
+    # export
+    os.chdir("../data/kaggle_data/lexicon")
+    base.to_csv("lexicon.wiegand.base.csv", header=0, index=False)
+    exp.to_csv("lexicon.wiegand.expanded.csv", header=0, index=False)
+    base_abusive.to_csv("lexicon.wiegand.base.abusive.csv", header=0, index=False)
+    exp_abusive.to_csv("lexicon.wiegand.expanded.abusive.csv", header=0, index=False)
+    os.chdir(cwd)  # go back to previous cwd
 
 
 """ MAIN """
@@ -112,3 +117,4 @@ to_build = "all"  # "all", "random", or "boosted"
 
 # manually run:
 # build_main(to_build, topic)
+# build_wiegand_lexicons()
