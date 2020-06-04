bb1cc4791d258074e33bbe8d462cacee992d57df
Dante Razo <dante@razonet.org>
2020-06-04 10:36:27 -0400

    Close comment block

--- Department/sklearn-demo/tuning_example.py
+++ Department/sklearn-demo/tuning_example.py
@@ -1,11 +1,12 @@
-#This is the same as classification_example.py for most of this
+# This is the same as classification_example.py for most of this
 import sklearn
 from sklearn.ensemble import RandomForestClassifier as rf
 from sklearn import datasets
 from sklearn.utils import shuffle
 from sklearn.metrics import classification_report
 from sklearn.model_selection import GridSearchCV
-#load dataset
+
+# load dataset
 wine_data = datasets.load_wine()
 
 X, Y = shuffle(wine_data["data"], wine_data["target"])
@@ -19,15 +20,15 @@ Wouldn't it be great if we could try out some parameters automatically?
 
 We can actually!
 """
-#First, you make an intialized model object
+# First, you make an intialized model object
 rf_model = rf(n_jobs=1)
 """
  We could very well just call fit on this and use the default settings. 
  However, this is generally a very bad idea. We can't really compare untuned classifiers
  Let's write some parameters that we can tune. 
 """
-params = {'n_estimators':[10, 20, 30, 100],
-          'criterion':['gini','entropy']}
+params = {'n_estimators': [10, 20, 30, 100],
+          'criterion': ['gini', 'entropy']}
 """
 The parameters that can be tuned are specific to each model type
 e.g. the options for random forest tuning are different from the ones for svm tuning
@@ -35,7 +36,7 @@ In the following line, we create a new model using our existing model, the list 
 and n_jobs which just specifies how many runs should be done in parallel
 cv specifies how many folds to do
 """
-model = GridSearchCV(rf_model, params, n_jobs=4, cv=5) 
+model = GridSearchCV(rf_model, params, n_jobs=4, cv=5)
 """
 Now we have an initialized model but no training has been conducted
 We need to call `fit()` 
@@ -50,7 +51,7 @@ train dataset
 Now we have a full model! Let's predict on the test dataset
 """
 preds = model.predict(test_X)
-#Let's print some results to see how we did!
+# Let's print some results to see how we did!
 print(classification_report(test_Y, preds, digits=6))
 """
 Look at the sklearn documentation and figure out how to print out
@@ -63,4 +64,4 @@ Change it to use an xgboost classifier or an svm.
 Do you see changes in performance? 
 What hyperparameters did you have to change to work with 
 the different classifier?
-
+"""
